9556619c945e08081e1a95c3415ace908c0475a0
lethanov <lethanov@gmail.com>
2016-03-31 20:01:15 +0200

    Personnage

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,22 +12,41 @@ public class Player : MonoBehaviour {
 	private Rigidbody2D rb;
 	private bool onGround;
 
+	private string _nexAnim = "Idle";
+	private string _curAnim = "";
+
+	private float initScale;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
+		initScale = transform.FindChild("Gfx").localScale.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		OnGround();
+		HandleAnimation();
 
 		xVel = Input.GetAxis("Horizontal") * speed;
+
+		if(xVel > 0){
+			transform.FindChild("Gfx").localScale = new Vector2(initScale, initScale);
+		} 
+		if(xVel < 0){
+			transform.FindChild("Gfx").localScale = new Vector2(-initScale, initScale);
+		}
 
 		if(onGround){
 			if(Input.GetButton("Jump")){
 				yVel = 20;
+				_nexAnim = "Jump";
 			} else {
 				yVel = 0;
+				_nexAnim = "Idle";
+				if(xVel != 0){
+					_nexAnim = "Run";
+				}
 			}
 		} else {
 			if(yVel < -20){
@@ -36,8 +55,15 @@ public class Player : MonoBehaviour {
 				yVel = rb.velocity.y;
 			}
 		}
-			
+
 		rb.velocity = new Vector2(xVel, yVel);
+	}
+
+	void HandleAnimation(){
+		if(_curAnim  != _nexAnim){
+			_curAnim = _nexAnim;
+			transform.FindChild("Gfx").GetComponent<Animator>().Play(_curAnim);
+		}
 	}
 
 	void OnGround() {
